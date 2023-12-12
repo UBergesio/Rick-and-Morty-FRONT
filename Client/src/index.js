9d0e1ee7cf8f9fc850e6eca2b7a7de494caefb61
UBergesio <ulibergesio@gmail.com>
2023-12-12 13:54:20 -0300

    subo6

--- Client/src/index.js
+++ Client/src/index.js
@@ -7,9 +7,9 @@ import store from './components/redux/store';
 import { Provider } from 'react-redux';
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001";
-/* axios.defaults.baseURL =
-  "https://rick-and-morty-back-production-2ae1.up.railway.app"; */
+/* axios.defaults.baseURL = "http://localhost:3001"; */
+axios.defaults.baseURL =
+  "https://rick-and-morty-back-production-2ae1.up.railway.app";
 
 ReactDOM.render(
   <Provider store={store}>
